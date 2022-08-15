ac5749823adafe39f7035d604f0921cdf01ba9d5
Robertas <rcekanauskasnl@gmail.com>
2022-08-15 05:39:04 +0300

    update colors

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Star } from "assets/icons";
 import Input from "components/Input";
-
+import { useState } from "react";
 function App() {
+  const [value, setValue] = useState("");
+  console.log(value);
   return (
     <div className="flex justify-center">
-      <Input Icon={Star} placeholder="test" label="Star" />
+      <Input
+        Icon={Star}
+        // value={value}
+        // setValue={setValue}
+        placeholder="test"
+        label="Star"
+      />
     </div>
   );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -3,19 +3,21 @@ import React from "react";
 export default function Input({ label, value, setValue, placeholder, Icon }) {
   let inputStyle = `border-slate-200 border-2 focus:border-slate-100 ${
     Icon && "pl-8"
-  } rounded-md py-2 bg-white text-gray-500 font-semibold`;
-  let iconStyle = "absolute gap-10 w-6 h-6 ml-3";
+  } rounded-md py-2 bg-white text-accentDark-500 font-semibold`;
+  let iconStyle = "absolute gap-10 fill-accentDark-200 w-6 h-6 ml-2";
+  let labelStyle = "text-lg font-semibold  text-accentDark-100";
 
+  const onChange = (e) => {
+    setValue && setValue(e.target.value);
+  };
   return (
     <div>
-      {label && (
-        <label className="text-md font-semibold text-gray-500">{label}</label>
-      )}
+      {label && <label className={labelStyle}>{label}</label>}
       <div className="flex items-center">
         {Icon && <Icon className={iconStyle} />}
         <input
           value={value}
-          setValue={setValue}
+          onChange={onChange}
           className={inputStyle}
           placeholder={placeholder && placeholder}
         />

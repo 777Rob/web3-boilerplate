2609edb1a4348cd41538042839274132942ca351
Robertas <rcekanauskasnl@gmail.com>
2022-08-15 08:15:56 +0300

    fix navbar

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import ThemeChange from "./ThemeChange";
 export default function Navbar() {
   return (
-    <div class="navbar bg-base-300 rounded-box">
+    <div class="navbar h-12 min-h-8 bg-base-300 rounded-box">
       <div class="flex-1 px-2 lg:flex-none">
         <a class="text-lg font-bold">App</a>
       </div>

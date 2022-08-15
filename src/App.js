fe43ddd28459a5c3f0bb2b647da68f074aeba43d
Robertas <rcekanauskasnl@gmail.com>
2022-08-15 06:58:52 +0300

    Add daisyUi

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./index.css";
 import { Star } from "assets/icons";
 import Input from "components/Input";
 import { useState } from "react";
-import Button from "components/Button";
 import Modal from "components/Modal";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
+
 function App() {
+  // https://github.com/saadeghi/theme-change
+  useEffect(() => {
+    themeChange(false);
+    // 👆 false parameter is required for react project
+  }, []);
   const [value, setValue] = useState(false);
+
   return (
     <div className="flex justify-center">
-      <Button warning onClick={() => setValue(!value)}>
-        Test
-      </Button>
+      <button class="btn">Button</button>
+      <button class="btn btn-primary">Button</button>
+      <button class="btn btn-secondary">Button</button>
+      <button class="btn btn-accent">Button</button>
+      <button class="btn btn-ghost">Button</button>
+      <button class="btn btn-link">Button</button>
       <Modal open={value} setOpen={setValue}>
         Test
       </Modal>
+      <button data-set-theme="cupcake" data-act-class="ACTIVECLASS">
+        emerald
+      </button>
     </div>
   );
 }

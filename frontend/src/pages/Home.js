145ce53d088c9d8e1b382d2c55fc63749983d60b
Robertas <rcekanauskasnl@gmail.com>
2022-09-02 18:38:49 +0300

    clean up

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -62,7 +62,6 @@ export default function Home() {
           </div>
         </div>
         <CreateLockButton config={config} />
-        {/* {isLoading ? <LoadingButton /> : <TestButton />} */}
       </div>
     </div>
   );

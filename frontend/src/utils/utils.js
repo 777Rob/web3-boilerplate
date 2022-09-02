7a72e0a1e5d888baae20ff9f0dda9fb20486869c
Robertas <rcekanauskasnl@gmail.com>
2022-09-03 01:19:30 +0300

    utils

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,7 +1,5 @@
-export const shortenAddress = (address) => {
-  return address.slice(0, 6) + "..." + address.slice(address.length - 4);
-};
+export const shortenAddress = (address) =>
+  address.slice(0, 6) + "..." + address.slice(address.length - 4);
 
-export const shortenTransactionHash = (hash) => {
-  return hash.slice(0, 12) + "....." + hash.slice(hash.length - 12);
-};
+export const shortenTransactionHash = (hash) =>
+  hash.slice(0, 12) + "....." + hash.slice(hash.length - 12);
